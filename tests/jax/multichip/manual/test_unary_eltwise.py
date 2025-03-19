9fef4076249a4028401314f97ca4215f902b88ba
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-03-19 09:08:07 +0000

    Added one test

--- tests/jax/multichip/manual/test_unary_eltwise.py
+++ tests/jax/multichip/manual/test_unary_eltwise.py
@@ -2,16 +2,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import jax
 import jax.numpy as jnp
 import pytest
 from infra import make_partition_spec, run_multichip_test_with_random_inputs
 
 
+@pytest.mark.n300
+@pytest.mark.push
 @pytest.mark.parametrize(
     ("input_shape", "mesh_shape", "axis_names"), [((256, 256), (1, 2), ("x", "y"))]
 )
-@pytest.mark.skip(reason=failed_fe_compilation("Multichip still in development"))
 def test_unary_eltwise(input_shape: tuple, mesh_shape: tuple, axis_names: tuple):
     def fwd(a_block):
         b_block = jnp.negative(a_block)
